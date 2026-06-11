85b6722696e8ed814cf83c48597d73325c3bcf1c
apple050620312 <apple050620312@gmail.com>
2026-06-12 03:53:51 +0800

    chore: remove linting and deployment config and implement ready event handler

--- src/events/ready.js
+++ src/events/ready.js
@@ -3,7 +3,7 @@ const logger = require('../utils/logger');
 const { ActivityType, PermissionsBitField } = require('discord.js');
 
 module.exports = {
-    name: 'ready',
+    name: 'clientReady',
     once: true,
     execute(client) {
         logger.info(`Bot has started as ${client.user.tag}`);
